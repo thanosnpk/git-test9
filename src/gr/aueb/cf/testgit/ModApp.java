c9bf7a4b52e554be76a7a46fd904a8593559fb66
thanosnpk <thanos.npk@gmail.com>
2025-12-24 20:24:17 +0200

    feat: add mod

---
+++ src/gr/aueb/cf/testgit/ModApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class ModApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int mod(int a, int b) {
+        return a % b;
+    }
+}
