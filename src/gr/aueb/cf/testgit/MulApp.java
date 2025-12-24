ad3752b42a2138ac69f88218cb7b99d63e81a4f8
thanosnpk <thanos.npk@gmail.com>
2025-12-24 20:09:35 +0200

    feat: ass mulapp

---
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class MulApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
