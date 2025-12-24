a0f1e159d49e251e4560494adbfece1d274b54ab
thanosnpk <thanos.npk@gmail.com>
2025-12-24 20:13:42 +0200

    feat: add divapp

---
+++ src/gr/aueb/cf/testgit/DivApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.testgit;
+
+public class DivApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) return 0;
+        return a / b;
+    }
+}
